44bc89d4793b4a168042a1c8e553230d89059e97
sol3896 <sol.imamai@strathmore.edu>
2024-06-13 21:36:59 +0300

    changed from .html to .php

--- templates/nav.php
+++ templates/nav.php
@@ -1,12 +1,12 @@
 <nav>
             <ul>
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="team.html">Team</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="feedback.html">Feedback</a></li>
-                <li><a href="product.html">Products</a></li>
-                <li><a href="register.html">Register</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="services.php">Services</a></li>
+                <li><a href="team.php">Team</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="feedback.php">Feedback</a></li>
+                <li><a href="product.php">Products</a></li>
+                <li><a href="register.php">Register</a></li>
             </ul>
         </nav>
