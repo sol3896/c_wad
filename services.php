1fc2f4f00b55d7d7433f828d4460c06e4b4efc79
sol3896 <sol.imamai@strathmore.edu>
2024-06-13 21:18:31 +0300

    added the nav reusability to the services page

--- services.php
+++ services.php
@@ -11,20 +11,8 @@
         <img src="images/SOL TECH SOLUTIONS.png" alt="SolTech Solutions Logo">
 
         <h1>Our Services</h1>
-        <nav>
-            <ul>                
-                <li><a href="index.html">Home</a></li>
-                <li><a href="about.html">About</a></li>
-                <li><a href="services.html">Services</a></li>
-                <li><a href="team.html">Team</a></li>
-                <li><a href="contact.html">Contact</a></li>
-                <li><a href="feedback.html">Feedback</a></li>
-                <li><a href="register.html">Register</a></li>
-                <li><a href="product.html">Products</a></li>
+        <?php include_once("templates/nav.php");?>
 
-
-            </ul>
-        </nav>
     </header>
 
     <main>
