1fd7a9e4bfd4229cd0c753d5fa29571ff89a575b
sol3896 <sol.imamai@strathmore.edu>
2024-06-13 21:31:14 +0300

    added the footer reusability in the services page

--- services.php
+++ services.php
@@ -79,8 +79,7 @@
         </section>
     </main>
 
-    <footer>
-        <p>&copy; 2024 SOL TECH SOLUTIONS. All rights reserved.</p>
-    </footer>
+    <?php include_once("templates/footer.php");?>
+
 </body>
 </html>
