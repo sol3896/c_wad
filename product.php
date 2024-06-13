fb060ec1ef747890b79c967019a037b1630b08ae
sol3896 <sol.imamai@strathmore.edu>
2024-06-13 21:30:09 +0300

    added the footer reusability in the product page

--- product.php
+++ product.php
@@ -82,8 +82,7 @@
         </div>
     </main>
 
-    <footer>
-        <p>&copy; 2024 SOL TECH SOLUTIONS. All rights reserved.</p>
-    </footer>
+    <?php include_once("templates/footer.php");?>
+
 </body>
 </html>
