f7b22506b260a974f5d632c238589efed479f7a8
sol3896 <sol.imamai@strathmore.edu>
2024-06-17 21:05:36 +0300

    created the sql queries

--- db.sql
+++ db.sql
@@ -1,1 +1,14 @@
-sql
+DROP DATABASE IF EXISTS sol;
+CREATE DATABASE IF NOT EXISTS sol;
+USE sol;
+
+DROP TABLE IF EXISTS feedback;
+CREATE TABLE IF NOT EXISTS feedback (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    message TEXT NOT NULL,
+    datecreated datetime not null default CURRENT_TIMESTAMP(),
+    updated_at datetime not null default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    UNIQUE KEY (email)
+);
