25022b6468e902ea891a1599f55432d3bc805f60
sol3896 <sol.imamai@strathmore.edu>
2024-06-19 20:25:17 +0300

    connected successfully to the database

--- dbconnections.php
+++ dbconnections.php
@@ -1,15 +1,14 @@
 <?php
 $servername = "localhost";
-$username = " root";
+$username = "root";
 $password = "";
 $db_name = "sol";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$db_name);
+$conn = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully";
-?>
