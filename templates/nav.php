33a1b525429f776a1aa62da6fa7a20944b821031
sol3896 <sol.imamai@strathmore.edu>
2024-07-07 13:57:27 +0300

    added view product to the nav

--- templates/nav.php
+++ templates/nav.php
@@ -11,6 +11,7 @@
                 <li><a href="view_messages.php">message</a></li>
                 <li><a href="view_feedback.php">FeedMessage</a></li>
                 <li><a href="view_users.php">ViewUser</a></li>
+                <li><a href="view_product.php">viewProduct</a></li>
 
 
             </ul>
